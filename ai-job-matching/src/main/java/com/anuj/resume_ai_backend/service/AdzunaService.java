aed28637654c79aa9b61c02c59bf612405392b63
Anuj Dutta <dattaanuj1804@gmail.com>
2026-03-09 12:22:12 +0530

    Show embedding error logs

--- ai-job-matching/src/main/java/com/anuj/resume_ai_backend/service/AdzunaService.java
+++ ai-job-matching/src/main/java/com/anuj/resume_ai_backend/service/AdzunaService.java
@@ -103,14 +103,14 @@ public class AdzunaService {
                 job.setApplyLink((String) jobData.get("redirect_url"));
 
                 try {
-                    job.setEmbedding(
-                            embeddingService.generateEmbedding(job.getDescription())
-                    );
-                } catch (Exception e) {
-                    System.out.println("Embedding failed, skipping job.");
-                    continue;
-                }
-
+    job.setEmbedding(
+        embeddingService.generateEmbedding(job.getDescription())
+    );
+} catch (Exception e) {
+    System.out.println("Embedding failed:");
+    e.printStackTrace();
+    continue;
+}
                 jobRepository.save(job);
                 imported++;
             }
