6332b6919741ce97eb7f4ce728ce867269eac28f
Anuj Dutta <dattaanuj1804@gmail.com>
2026-03-09 12:55:58 +0530

    Token issue resolved

--- ai-job-matching/src/main/java/com/anuj/resume_ai_backend/ai/EmbeddingService.java
+++ ai-job-matching/src/main/java/com/anuj/resume_ai_backend/ai/EmbeddingService.java
@@ -3,6 +3,8 @@ package com.anuj.resume_ai_backend.ai;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.*;
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.*;
 
 @Service
@@ -10,17 +12,21 @@ public class EmbeddingService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${HF_TOKEN}")
+    private String hfToken;
+
     public String generateEmbedding(String text) {
 
         String url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(hfToken);
 
-        String token = System.getenv("ai-job-matcher");
-        headers.setBearerAuth(token);
+        Map<String, String> request = new HashMap<>();
+        request.put("inputs", text);
 
-        HttpEntity<String> entity = new HttpEntity<>(text, headers);
+        HttpEntity<Map<String, String>> entity = new HttpEntity<>(request, headers);
 
         List<List<Double>> response =
                 restTemplate.postForObject(url, entity, List.class);
